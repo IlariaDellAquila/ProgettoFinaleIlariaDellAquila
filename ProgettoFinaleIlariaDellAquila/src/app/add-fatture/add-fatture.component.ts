654f68f023403740e52d9bfa50cae9de6025e5c0
IlariaDellAquila <ilaridelli@hotmail.it>
2022-01-27 19:02:09 +0100

    Aggiornamento

--- ProgettoFinaleIlariaDellAquila/src/app/add-fatture/add-fatture.component.ts
+++ ProgettoFinaleIlariaDellAquila/src/app/add-fatture/add-fatture.component.ts
@@ -12,20 +12,27 @@ import { FattureService } from '../services/fatture.service';
   styleUrls: ['./add-fatture.component.css']
 })
 export class AddFattureComponent implements OnInit {
-
   fattura: Fattura = new Fattura();
+
+  /*fatture = {"data":"2019-07-31T16:09:43.763+00:00","stato":{
+    "id": 2,
+    "nome": "NON PAGATA"
+  },
+  "cliente":{"id":1}}*/
 
   constructor(private route: ActivatedRoute, private fattureService: FattureService, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(data=> {
+      this.fattura.cliente.id = data['id']; 
+       })
   }
 
   addFattura() {
-    this.fattura.stato.nome='NON PAGATO';
-    this.fattura.data = '2019-06-01T08:11:01.911+00:00';
-    this.fattureService.createFattura(this.fattura).subscribe(data => {
-     this.fattura = data;
-    })
+   this.fattura.data = "2019-07-31T16:09:43.763+00:00";
+   this.fattura.stato.id = 2;
+   this.fattura.stato.nome = "NON PAGATA";
+    this.fattureService.createFattura(this.fattura).subscribe();
     this.router.navigate(['fatture']);
   }
 
